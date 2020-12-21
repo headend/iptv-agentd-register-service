d89f6bedbd23aa90dd4c149cf8bada86df5a5e5d
thuonghh <thuonghh@vng.com.vn>
2020-12-21 14:42:11 +0700

    service to add agent if not exists

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/headend/iptv-agentd-register-service
+
+go 1.13
+
+require (
+	github.com/headend/iptv-agent-service v0.0.0-20201220144429-f50ba245ec25 // indirect
+	github.com/headend/share-module v0.0.0-20201220164436-3f397c07bfcd
+)
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	messagequeue "github.com/headend/share-module/MQ"
 	"github.com/headend/share-module/configuration"
+	"github.com/headend/share-module/model/register"
 	"log"
+	agentpb "github.com/headend/iptv-agent-service/proto"
+	"time"
+	myRpc"github.com/headend/share-module/mygrpc/connection"
 )
 
 func main()  {
@@ -16,7 +22,14 @@ func main()  {
 	if mq.Err != nil {
 		log.Print(mq.Err)
 	}
+
 	log.Printf("Listen mesage from %s topic\n",conf.MQ.WarmUpTopic)
+	var agentConn myRpc.RpcClient
+	//try connect to agent
+	agentConn.InitializeClient(conf.RPC.Agent.Host, string(conf.RPC.Agent.Port))
+	defer agentConn.Client.Close()
+	//	connect agent services
+	agentClient := agentpb.NewAgentCTLServiceClient(agentConn.Client)
 	for {
 		msg, err := mq.Consumer.ReadMessage(-1)
 		if err != nil {
@@ -24,8 +37,33 @@ func main()  {
 			log.Print("Se you again!")
 			break
 		}
-		log.Print(msg.value)
-		//var controlMsgData *model.AgentCTLQueueRequest
-		//json.Unmarshal(msg.Value, &controlMsgData)
+		log.Print(msg.Value)
+		var registerMsgData *register.Register
+		json.Unmarshal(msg.Value, &registerMsgData)
+
+		// Check Agent exists
+		c, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		res, err := (agentClient).Get(c, &agentpb.AgentFilter{IpControl: registerMsgData.IP})
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		if len(res.Agents) > 0 {
+			log.Println("Agent already exists")
+			continue
+		}
+		//
+		// create agent
+		newAgent := agentpb.AgentRequest{
+			IpControl:          registerMsgData.IP,
+			IpReceiveMulticast: "",
+			Status:             true,
+		}
+		_, err2 := (agentClient).Add(c, &newAgent)
+		if err2 != nil {
+			log.Println(err2)
+			continue
+		}
 	}
 }
